a021a4c52a640efe1ca667969d2352c7bac535ce
Thomas L Randall <tlranda@clemson.edu>
2023-05-08 14:21:21 -0400

    Update figure

--- ytopt/benchmark/ConditionalSampling/constrained_task_3mm.py
+++ ytopt/benchmark/ConditionalSampling/constrained_task_3mm.py
@@ -22,18 +22,29 @@ def sdv_constrained(x, low, high):
     return np.log(x / (1.0 - x))
 
 fig,ax = plt.subplots()
+size_lookup = {}
 for size in sizes[1:-1]:
     name = lookup_ival[size][1]
     if len(name) > 2:
         name = name.lower().capitalize()
     constrained = sdv_constrained(size, constraint_low, constraint_high)
+    size_lookup[size] = constrained
     ax.scatter(size, constrained, label=f"{name} Task")
     print(f"{name} --> {constrained}")
 ax.legend(loc='upper left')
-ax.plot(x_range, sdv_constrained(x_range, constraint_low, constraint_high), label="Logit Values", zorder=-1)
+ax.plot(x_range, sdv_constrained(x_range, constraint_low, constraint_high), label="Logit Values", zorder=-2)
+# Linear Fit line
+lin_min = sdv_constrained(x_range[0],constraint_low,constraint_high)
+lin_max = sdv_constrained(x_range[-1], constraint_low, constraint_high)
+lin_range = ((lin_max-lin_min)/(constraint_high-constraint_low)) * x_range - (lin_max-lin_min)/2
+ax.plot(x_range, lin_range, label="Linear Fit", linestyle='dotted', color='k', zorder=-1)
+#for size in sizes[1:-1]:
+#    ax.vlines(size, min(size_lookup[size], lin_range[size]), max(size_lookup[size], lin_range[size]), zorder=-3, color='k')
+ax.grid(axis='y')
 #ax.set_xscale('log')
 ax.set_xlabel('Task Size')
 ax.set_ylabel('Constraint Value')
 ax.set_title(f"3mm constraints between {constraint_low} and {constraint_high}")
+fig.tight_layout()
 fig.savefig(os.path.dirname(__file__)+"/Assets/constrained_values.png", format='png')
 
